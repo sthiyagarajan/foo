8025d54ccd586583352352e87f24d313efae774c
Saru Thiyagarajan <saru@nuodb.(none)>
2012-12-21 08:30:48 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foodemo::Application.config.secret_token = 'a87347ead24aa8bd49de3b3011a4eebaf5f4d0492f006b9b0059c4177ee96a3a32829b609199da175ade5447f7a779491febed54a194ac707bc811d9c25d3e24'
